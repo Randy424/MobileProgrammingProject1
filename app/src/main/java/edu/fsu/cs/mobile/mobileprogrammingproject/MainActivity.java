c7e990c098d842a876f398dab5b1d0b7c0f2f144
Unknown <jdm02k@my.fsu.edu>
2018-03-23 21:19:01 -0400

    toasting our db name

--- app/src/main/java/edu/fsu/cs/mobile/mobileprogrammingproject/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/mobileprogrammingproject/MainActivity.java
@@ -3,15 +3,24 @@ package edu.fsu.cs.mobile.mobileprogrammingproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    static final String FIREBASE_TABLE = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent i = new Intent(MainActivity.this, MapsActivity.class);
-        startActivity(i);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        Toast.makeText(this, myRef.toString(), Toast.LENGTH_LONG).show();
+        //DatabaseReference myRef = database.getReference(FIREBASE_TABLE);
+        //Intent i = new Intent(MainActivity.this, MapsActivity.class);
+        //startActivity(i);
     }
 }
