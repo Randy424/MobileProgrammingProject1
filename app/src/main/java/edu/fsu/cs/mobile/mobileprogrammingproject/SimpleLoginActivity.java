3e20e825907cc12661de4c74b64d70041e165f74
Aaron <aaronx07@yahoo.com>
2018-03-24 18:38:30 -0400

    Simplified Login ready for testing.

--- app/src/main/java/edu/fsu/cs/mobile/mobileprogrammingproject/SimpleLoginActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/mobileprogrammingproject/SimpleLoginActivity.java
@@ -1,5 +1,6 @@
-package edu.fsu.cs.mobile.mobileprogrammingproject.dummy;
+package edu.fsu.cs.mobile.mobileprogrammingproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import edu.fsu.cs.mobile.mobileprogrammingproject.R;
@@ -32,6 +34,9 @@ public class SimpleLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         loginButton = (Button) findViewById(R.id.email_sign_in_button);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.setValue("TEST WORKED");
+        Toast.makeText(this, mDatabase.toString(), Toast.LENGTH_SHORT).show();
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,22 +45,32 @@ public class SimpleLoginActivity extends AppCompatActivity {
                 mPassword =(EditText) findViewById(R.id.password);
                 if(Verify(mEmail.getText().toString(), "email"))
                     Toast.makeText(getApplicationContext(), "It's a match!", Toast.LENGTH_SHORT).show();
-                Verify(mPassword.getText().toString(), "password");
+                //Verify(mPassword.getText().toString(), "password");
+            }
+        });
+
+        Button registerButton = (Button) findViewById(R.id.registerBtn);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SimpleLoginActivity.this, RegisterActivity.class);
+                startActivity(i);
             }
         });
 
 
     }
 
-    boolean Verify(String field, final String key)
+    boolean Verify(String field, String key)
     {
+        final String inKey = key;
         final String entry = field; //converting to final allows for inner class usage.
         verified = false;
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot messageSnapshot: dataSnapshot.getChildren()) {
-                    if(entry.equals((String) messageSnapshot.child(key).getValue()))
+                    if(entry.equals((String) messageSnapshot.child(inKey).getValue()))
                         verified = true;
                     //String name = (String) messageSnapshot.child("name").getValue();
                     //String message = (String) messageSnapshot.child("message").getValue();
