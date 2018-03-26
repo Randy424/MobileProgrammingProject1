54ad111e9cf1ed73cc8f4f89eb1989222c33c5af
Unknown <jdm02k@my.fsu.edu>
2018-03-25 21:22:45 -0400

    Newest changes, does tracking on signin or register

--- app/src/main/java/edu/fsu/cs/mobile/mobileprogrammingproject/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/mobileprogrammingproject/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -81,9 +82,14 @@ public class MainActivity extends AppCompatActivity {
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                MapsActivity.dbLatLngs.clear();
+                //MapsActivity.updateMarkers(dataSnapshot);
                 //String name = (String) messageSnapshot.child("name").getValue();
                 //String message = (String) messageSnapshot.child("message").getValue();
                 for(DataSnapshot messageSnapshot : dataSnapshot.getChildren() ){
+                    MapsActivity.dbLatLngs.add(new LatLng(Double.parseDouble(messageSnapshot.child("latitude").getValue().toString()), Double.parseDouble(messageSnapshot.child("longitude").getValue().toString())));
+
+
                 userList.put((String) messageSnapshot.child("phone").getValue(), new User((String) messageSnapshot.child("name").getValue(),
                         (String) messageSnapshot.child("email").getValue(),(String) messageSnapshot.child("password").getValue(),(String) messageSnapshot.child("longitude").getValue(),
                         (String) messageSnapshot.child("latitude").getValue()));
