0cc59d8fc4285379800b4f4cba29a5c06f866863
Unknown <jdm02k@my.fsu.edu>
2018-04-13 23:59:59 -0400

    Cleanup what I could

--- app/src/main/java/edu/fsu/cs/mobile/mobileprogrammingproject/Fragments/BlogFeedFragment.java
+++ app/src/main/java/edu/fsu/cs/mobile/mobileprogrammingproject/Fragments/BlogFeedFragment.java
@@ -3,7 +3,6 @@ package edu.fsu.cs.mobile.mobileprogrammingproject.Fragments;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -16,17 +15,13 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Queue;
+import java.util.Objects;
 
 import edu.fsu.cs.mobile.mobileprogrammingproject.Posts;
 import edu.fsu.cs.mobile.mobileprogrammingproject.R;
@@ -50,25 +45,13 @@ public class BlogFeedFragment extends Fragment {
     RecylerViewAdapter recylerViewAdapter;
 
 
-    private FirebaseDatabase database;
-    private DatabaseReference databaseRef;
-    private FirebaseAuth mAuth;
-    private DatabaseReference mDatabaseUsers;
-    private FirebaseUser mCurrentUser;
-
-    private FirebaseFirestore db;
-
-
-    private OnFragmentInteractionListener mListener;
-
     public BlogFeedFragment() {
         // Required empty public constructor
     }
 
 
     public static BlogFeedFragment newInstance() {
-        BlogFeedFragment fragment = new BlogFeedFragment();
-        return fragment;
+        return new BlogFeedFragment();
     }
 
     @Override
@@ -79,7 +62,7 @@ public class BlogFeedFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_blog_feed, container, false);
         rootView.setBackgroundColor(Color.WHITE);
@@ -91,7 +74,7 @@ public class BlogFeedFragment extends Fragment {
         desc = new ArrayList<>();
         time = new ArrayList<>();
         email = new ArrayList<>();
-        db = FirebaseFirestore.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         try {
             Log.d(TAG, "Reached Try");
@@ -111,7 +94,6 @@ public class BlogFeedFragment extends Fragment {
 
                                     Log.d("L Content", document.getId() + " => " + l.getTitle());
 
-                                    //  entries.add(l);
                                     title.add(l.getTitle());
                                     desc.add(l.getDesc());
                                     time.add(l.getTime().toString());
@@ -124,7 +106,7 @@ public class BlogFeedFragment extends Fragment {
                                 timeFinal = time.toArray(new String[time.size()]);
                                 emailFinal = email.toArray(new String[email.size()]);
 
-                                recyclerView = (RecyclerView) getView().getRootView().findViewById(R.id.cardView);
+                                recyclerView = Objects.requireNonNull(getView()).getRootView().findViewById(R.id.cardView);
                                 layoutManager = new LinearLayoutManager(getActivity());
                                 recyclerView.setLayoutManager(layoutManager);
                                 recylerViewAdapter = new RecylerViewAdapter(getActivity(), titleFinal, descFinal, timeFinal, emailFinal);
@@ -137,31 +119,19 @@ public class BlogFeedFragment extends Fragment {
                         }
                     });
         } catch (Exception e) {
-            messageBox("doStuff", e.getMessage());
+            messageBox(e.getMessage());
             Log.d(TAG, "Error getting documents: ");
         }
 
-        if (getArguments() != null) {
-        }
-
-
-        // LinearLayoutManager llm = new LinearLayoutManager(getContext());
-        //  rv.setLayoutManager(llm);
 
         return rootView;
-    }
-
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        //noinspection StatementWithEmptyBody
         if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -171,23 +141,21 @@ public class BlogFeedFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
     public interface OnFragmentInteractionListener {
 
-        void onFragmentInteraction(Uri uri);
     }
 
     // This method creates an ArrayList that has three Person objects
 // Checkout the project associated with this tutorial on Github if
 // you want to use the same images.
 
-    private void messageBox(String method, String message) {
-        Log.d("EXCEPTION: " + method, message);
+    private void messageBox(String message) {
+        Log.d("EXCEPTION: " + "doStuff", message);
 
         AlertDialog.Builder messageBox = new AlertDialog.Builder(getActivity());
-        messageBox.setTitle(method);
+        messageBox.setTitle("doStuff");
         messageBox.setMessage(message);
         messageBox.setCancelable(false);
         messageBox.setNeutralButton("OK", null);
--- app/src/main/java/edu/fsu/cs/mobile/mobileprogrammingproject/Fragments/BlogPostFragment.java
+++ app/src/main/java/edu/fsu/cs/mobile/mobileprogrammingproject/Fragments/BlogPostFragment.java
@@ -1,29 +1,26 @@
 package edu.fsu.cs.mobile.mobileprogrammingproject.Fragments;
 
 import android.content.Context;
-import android.content.Intent;
+
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import edu.fsu.cs.mobile.mobileprogrammingproject.Posts;
 import edu.fsu.cs.mobile.mobileprogrammingproject.R;
@@ -31,14 +28,10 @@ import edu.fsu.cs.mobile.mobileprogrammingproject.R;
 
 public class BlogPostFragment extends Fragment {
 
-    private ImageButton imageBtn;
     private FirebaseFirestore db;
-    //    private static final int GALLERY_REQUEST_CODE = 2;
-    private Uri uri = null;
+
     private EditText textTitle;
     private EditText textDesc;
-    private Button postBtn;
-
 
     private OnFragmentInteractionListener mListener;
 
@@ -57,21 +50,18 @@ public class BlogPostFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_blog_post, container, false);
         // Inflate the layout for this fragment
         db = FirebaseFirestore.getInstance();
-        Button myButton = (Button) rootView.findViewById(R.id.submit);
-//        View imageButton = rootView.findViewById(R.id.imageButton);
-        textTitle = (EditText) rootView.findViewById(R.id.title);
-        textDesc = (EditText) rootView.findViewById(R.id.desc);
+        Button myButton = rootView.findViewById(R.id.submit);
+
+        textTitle = rootView.findViewById(R.id.title);
+        textDesc = rootView.findViewById(R.id.desc);
         rootView.setBackgroundColor(Color.WHITE);
         rootView.setClickable(true);
 
@@ -85,14 +75,12 @@ public class BlogPostFragment extends Fragment {
                     case R.id.submit: {
                         //Do error checking too
 
-                        // Toast.makeText(getActivity(), "POSTING...", Toast.LENGTH_LONG).show();
                         String PostTitle = textTitle.getText().toString().trim();
                         String PostDesc = textDesc.getText().toString().trim();
-                        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
-                        //String usersEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+                        String email = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getEmail();
+
 
                         Toast.makeText(getActivity(), "POSTING..." + PostTitle, Toast.LENGTH_LONG).show();
-                        Map<String, Object> messageIdMap = new HashMap<>();
                         Posts myPost = new Posts(PostTitle, PostDesc, email);
 
                         db.collection("Blog_Post")
@@ -107,12 +95,6 @@ public class BlogPostFragment extends Fragment {
         });
 
         return rootView;
-    }
-
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
@@ -133,7 +115,5 @@ public class BlogPostFragment extends Fragment {
     }
 
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
